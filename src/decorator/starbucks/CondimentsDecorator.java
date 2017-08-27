cc7e166406e845cd9dbd817921c1631f46d19003
albertowusuasare <owusuasa@grinnell.edu>
2017-08-27 11:38:04 -0400

    Fix indentation errors

--- src/decorator/starbucks/CondimentsDecorator.java
+++ src/decorator/starbucks/CondimentsDecorator.java
@@ -2,22 +2,14 @@ package decorator.starbucks;
 
 public abstract class CondimentsDecorator extends Beverage {
     private Beverage beverage;
-
-    CondimentsDecorator(Beverage beverage){
-        this.beverage = beverage;
-    }
+    
+    CondimentsDecorator(Beverage beverage){ this.beverage = beverage;}
 
     @Override
-    public String getDescription(){
-        return (beverage != null) ? beverage.getDescription() : "";
-    }
+    public String getDescription(){ return (beverage != null) ? beverage.getDescription() : ""; }
 
     @Override
-    public double getCost(){
-        return (beverage != null) ? this.beverage.getCost() : Double.MIN_VALUE;
-    }
+    public double getCost(){ return (beverage != null) ? this.beverage.getCost() : Double.MIN_VALUE;}
 
-    public void setBeverage(Beverage beverage){
-        this.beverage = beverage;
-    }
+    public void setBeverage(Beverage beverage){ this.beverage = beverage; }
 }
